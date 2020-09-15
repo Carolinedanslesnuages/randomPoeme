4f80ab84eabc0bb6f7bb562e5f1e762229f73d26
Caroline Robillard <45307611+Carolinedanslesnuages@users.noreply.github.com>
2020-09-15 23:39:51 +0200

    Create random.js

---
+++ random.js
@@ -0,0 +1,28 @@
+const strophe = [
+'Insidieusement',
+'Les joues empourprées',
+'Mes yeux dans les tiens',
+'Un désir secret'
+]
+const stropheUn = [
+"Une envie soyeuse",
+'Un sourire malicieux',
+'De ta peau contre ma peau',
+'Un doux rêve me surprends'
+]
+const stropheDeux = [
+"Mon corps se replis", 
+"Se couche sur moi",
+"Mon cœur s'éveille",
+'Ta bouche brumeuse'
+]
+
+const random = Math.floor(Math.random() * strophe.length);
+
+
+strophe[random]
+stropheUn[random]
+stropheDeux[random]
+
+
+
